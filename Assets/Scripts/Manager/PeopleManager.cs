513cebffd5ab751c51ddef3bdc54aa70df9ae655
Choi Jeongho <jeonghoe22@gmail.com>
2025-10-02 00:03:01 +0900

    feat : 남은 인원 수에 따른 상호작용 추가

--- Assets/Scripts/Manager/PeopleManager.cs
+++ Assets/Scripts/Manager/PeopleManager.cs
@@ -13,6 +13,7 @@ public class PeopleManager : MonoBehaviour
     [SerializeField] AreaAnchor[] AreaAnchors;
     [SerializeField] AreaZone[] AreaZones;
 
+    public event System.Action OnAreaPeopleCountChanged;            // 구역에 있는 사람 변경
 
     void Awake()
     {
@@ -31,6 +32,7 @@ public class PeopleManager : MonoBehaviour
         if (!actor) return;
         var area = ResolveArea(actor.transform);
         _areaSets[area].Add(actor);
+        OnAreaPeopleCountChanged?.Invoke();
     }
 
     /// <summary>명시적 AreaType으로 등록 (부모 체인 무시)</summary>
@@ -38,6 +40,7 @@ public class PeopleManager : MonoBehaviour
     {
         if (!actor) return;
         _areaSets[area].Add(actor);
+        OnAreaPeopleCountChanged?.Invoke();
     }
 
     /// <summary>어느 영역에 있든 안전하게 해제</summary>
@@ -47,6 +50,7 @@ public class PeopleManager : MonoBehaviour
         // 혹시 캐시가 없다면 전 영역에서 제거 (HashSet이면 비용 작음)
         foreach (var set in _areaSets.Values)
             set.Remove(actor);
+        OnAreaPeopleCountChanged?.Invoke();
     }
 
     /// <summary>부모 변경 등으로 영역이 바뀐 경우 호출</summary>
