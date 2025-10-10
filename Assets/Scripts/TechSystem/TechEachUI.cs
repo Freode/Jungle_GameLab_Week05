5a3483a847a67c4bd6c2a16d4290dcbe546c899d
Choi Jeongho <jeonghoe22@gmail.com>
2025-10-10 16:20:36 +0900

    feat : 구조물을 처음으로 업그레이드할 때, 받을 수 있도록 효과 추가

--- Assets/Scripts/TechSystem/TechEachUI.cs
+++ Assets/Scripts/TechSystem/TechEachUI.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem.HID;
 using UnityEngine.UI;
 
 public class TechEachUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
