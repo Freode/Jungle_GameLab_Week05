42d9727e3ab72d2207a782d392f713e0c31dfeb2
kcwww <cwkim0321@gmail.com>
2025-10-11 21:35:37 +0900

    fix: authority manager

--- Assets/Scripts/Click/ClickThrottle.cs
+++ Assets/Scripts/Click/ClickThrottle.cs
@@ -90,7 +90,7 @@ public class ClickThrottle : MonoBehaviour
     {
         if (TryClick() == false)
             return;
-        //AuthorityManager.instance.IncreaseAuthority();
+        AuthorityManager.instance.IncreaseAuthority();
         // 공식 : 선형 증가량 * 비율 증가량
         long totalAmount = GameManager.instance.GetClickIncreaseTotalAmount();
 
