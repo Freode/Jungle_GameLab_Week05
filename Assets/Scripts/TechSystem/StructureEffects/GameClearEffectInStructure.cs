3fa4339586b37b19a9b26f43077ea6d3c0b27b75
Choi Jeongho <jeonghoe22@gmail.com>
2025-10-13 22:05:39 +0900

    feat : 피라미드를 망치로 모두 완성해야 게임 클리어되도록 수정

---
+++ Assets/Scripts/TechSystem/StructureEffects/GameClearEffectInStructure.cs
@@ -0,0 +1,16 @@
+using Mono.Cecil;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameClearEffect", menuName = "Scriptable Objects/Structure Effect/Game Clear Effect")]
+public class GameClearEffectInStructure : BaseStructureEffect
+{
+    public Action<TechData> OnClearEvent;
+    public TechData techData;
+
+    public override string ApplyTechEffect()
+    {
+        OnClearEvent?.Invoke(techData);
+        return string.Empty;
+    }
+}
